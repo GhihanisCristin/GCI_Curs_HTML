4aa0bd0923c9ac19bacfaad04352762f8a4d8326
Student <student@stepit.loc>
2018-11-10 11:54:30 +0200

    De la Step IT

---
+++ gallery-start/gallery-start/main.js
@@ -0,0 +1,19 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+for (var i = 1; i<=5; i++){
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', "images/pic" + i + ".jpg");
+  thumbBar.appendChild(newImage);
+}
+/* Wiring up the Darken/Lighten button */
+var button = document.querySelector('#dark');
+button.addEventListener("click", ()=>{
+  /*alert('Tup!!');*/
+  document.querySelector('.overlay').setAttribute('style', 'background-color:rgba(150,150,150,0.6)');
+})
+
